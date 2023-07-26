fec89b5e1565777d9852e26e6226478db0fd95ff
Abiachieve <abinaya.achieve123@gmail.com>
2023-07-26 12:53:56 +0530

    contentupdate

---
+++ script.js
@@ -0,0 +1,29 @@
+//var countdown=10;
+//var i=0;
+//while(i<countdown)
+//{
+ //   console.log(countdown);
+ //   countdown--;
+//}
+// console.log(Math.round(6.6));
+// console.log(Math.round(5.4));
+// console.log(Math.sqrt(36));
+// console.log(Math.sqrt(-36));
+// console.log(Math.sqrt(0));
+// console.log(Math.cbrt(27));
+// console.log(Math.cbrt(125));
+// console.log(Math.cbrt(-27));
+// console.log(Math.cbrt(0));
+// 
+// console.log(Math.trunc(27));
+// console.log(Math.trunc(125.8));
+// console.log(Math.trunc(-27.6));
+// console.log(Math.min(27,6,89,34));
+// console.log(Math.min(125.8,6,89,34));
+// console.log(Math.min(-27.6,6,89,34));
+// console.log(Math.max(27,6,89,34));
+// console.log(Math.max(125.8,6,89,34));
+// console.log(Math.max(-27.6,6,89,34));
+
+console.log(Math.pow(2,5));
+console.log(Math.random()*5);
